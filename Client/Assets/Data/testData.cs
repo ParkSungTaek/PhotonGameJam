849a0c920176b70a37e65198443e85d8c40b823d
seonjung <kvg753@gmail.com>
2024-07-27 19:58:30 +0900

    데이터테이블 제네레이터 추가

--- Client/Assets/Data/testData.cs
+++ Client/Assets/Data/testData.cs
@@ -1,10 +1,17 @@
 
 
+using System.Collections.Generic;
+
 namespace Client
 {
 	public class testData : SheetData
 	{
 		int ID; // 아이디
 		string name; // 이름
-	}
+
+        public override Dictionary<int, SheetData> LoadData()
+        {
+            return new Dictionary<int, SheetData>();
+        }
+    }
 }
---
+++ Client/Assets/Scripts/DataSheets/TestData.cs
@@ -0,0 +1,64 @@
+using Client;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using ExcelDataReader;
+using System.Data;
+
+public class TestData : SheetData
+{
+
+	public int index; // 인덱스
+	
+	public int attack; // 공격력
+	
+	public string name; // 이름
+	
+
+    public override Dictionary<int, SheetData> LoadData()
+    {
+        var dataList = new Dictionary<int, SheetData>();
+
+        string filePath = $"Assets/Data/XLSXS/{this.GetType().Name}.xlsx";
+        using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+        {
+            using (var reader = ExcelReaderFactory.CreateReader(stream))
+            {
+                var result = reader.AsDataSet();
+                DataTable table = result.Tables[0];
+
+                for (int rowIndex = 3; rowIndex <= table.Rows.Count - 1; rowIndex++)
+                {
+                    DataRow row = table.Rows[rowIndex];
+                    TestData data = new TestData();
+
+
+					if (row[0] != DBNull.Value)
+					{
+					    data.index = Convert.ToInt32(row[0]);
+					}
+					
+					
+					if (row[1] != DBNull.Value)
+					{
+					    data.attack = Convert.ToInt32(row[1]);
+					}
+					
+					
+					if (row[2] != DBNull.Value)
+					{
+					    data.name = Convert.ToString(row[2]);
+					}
+					
+					
+
+                    dataList[data.index] = data;
+                }
+            }
+        }
+
+        return dataList;
+    }
+}
