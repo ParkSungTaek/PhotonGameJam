7b5f45b449aebf0a803b2b279361a1ce87c86cca
ParkSungTaek <sbag8487@gmail.com>
2024-07-27 14:51:14 +0900

    박성택 : 버프 골격 Player 타입 추가

--- Client/Assets/Scripts/System/Entity/EntityBase.cs
+++ Client/Assets/Scripts/System/Entity/EntityBase.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Client
 {
+    /// <summary>
+    /// 객체의 기초 Base 클래스 
+    /// Awake 쓸 때 반드시 base.Awake상속할 것
+    /// </summary>
     public abstract class EntityBase : MonoBehaviour
     {
         protected long _ID;
@@ -14,8 +19,25 @@ namespace Client
             _ID = EntityManager.Instance.GetNextID();
         }
 
+        /// <summary>
+        /// 엔티티 메니저에 Set 
+        /// </summary>
+        private void Awake()
+        {
+            Type type = this.GetType();
+            EntityManager.Instance.SetEntity(type ,this);
+        }
+
         public long GetID() => _ID;
         public SystemEnum.EntityType GetEntityType() => _EntityType;
+
+        // EntityManager 에서 제거 및 삭제
+        public virtual void RemoveEntity()
+        {
+            Type type = this.GetType();
+            EntityManager.Instance.RemoveEntity(type, this);
+            Destroy(this.gameObject);
+        }
 
     }
 }
